42a5b83ea36f7823790ae56b5c8c15e97dd3b5e2
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-07-09 21:53:41 +0200

    added master build scripts 3

--- aws_build_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker network create --attachable -d macvlan  --subnet=10.2.0.0/16  --gateway=10.2.0.1  -o parent=$1 inethi-bridge
-
-./mysql/local_build.sh
-./nextcloud/local_build.sh
-./avideo/local_build.sh
-./plex/localbuild.sh $2
