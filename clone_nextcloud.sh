5140cae0dfc45a0c2b07c6ec6e0fb93b7ddc8530
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-07-13 14:01:35 +0200

    added fix to stop rclone filling optimzed plex videos

--- clone_nextcloud.sh
+++ clone_nextcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
   
-rclone -v  sync webdav:/Rshare /mnt/data/nextcloud/Rshare/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
-rclone -v  sync webdav:/Rvideo /mnt/data/nextcloud/Rvideo/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
-rclone -v  sync webdav:/Rmusic /mnt/data/nextcloud/Rmusic/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
+rclone --filter-from /etc/inethi/rclone/exclude-file.txt -v  sync webdav:/Rshare /mnt/data/nextcloud/Rshare/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
+rclone --filter-from /etc/inethi/rclone/exclude-file.txt -v  sync webdav:/Rvideo /mnt/data/nextcloud/Rvideo/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
+rclone --filter-from /etc/inethi/rclone/exclude-file.txt -v  sync webdav:/Rmusic /mnt/data/nextcloud/Rmusic/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
