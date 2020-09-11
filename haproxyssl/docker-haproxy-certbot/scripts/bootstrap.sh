f70d1dc85bc3604281482ce61c92ab371fdd63bf
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-09-11 19:57:08 +0200

    adding Dockerfile haproxyssl 2

---
+++ haproxyssl/docker-haproxy-certbot/scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# save container environment variables to use it
+# in cron scripts
+
+declare -p | grep -Ev '^declare -[[:alpha:]]*r' > /container.env
+
+# go!
+
+#/certs.sh && supervisord -c /etc/supervisord.conf -n
+supervisord -c /etc/supervisord.conf -n
