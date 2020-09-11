13eec21a83961acfa739546d1255a359e8a82bbf
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-09-11 20:45:24 +0200

    fixed haproxyssl env constants 3

--- haproxyssl/docker-haproxy-certbot/scripts/certs.sh
+++ haproxyssl/docker-haproxy-certbot/scripts/certs.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -n "$CERTS" ]; then
-    certbot certonly --no-self-upgrade -n --text --standalone \
-        --preferred-challenges http-01 \
-        -d "$CERTS" --keep --expand --agree-tos --email "$EMAIL" \
-        || exit 1
+certbot certonly --manual --no-self-upgrade --keep \
+ --preferred-challenges=dns --email inethi4us@gmail.com \
+ --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.inethi.net
 
-    mkdir -p /etc/haproxy/certs
-    for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
-        cat /etc/letsencrypt/live/$site/privkey.pem \
-          /etc/letsencrypt/live/$site/fullchain.pem \
-          | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
-    done
-fi
+mkdir -p /etc/haproxy/certs
+for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
+    cat /etc/letsencrypt/live/$site/privkey.pem \
+        /etc/letsencrypt/live/$site/fullchain.pem \
+        | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
+done
+
 
 exit 0
