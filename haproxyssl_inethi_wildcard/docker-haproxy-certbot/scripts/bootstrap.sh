0ea5c7a7104e10c6bf4668d0e2d92ab446475710
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-09-14 21:49:06 +0200

    added wildcard prebuilt haproxyssl 2

--- haproxyssl_inethi_wildcard/docker-haproxy-certbot/scripts/bootstrap.sh
+++ haproxyssl_inethi_wildcard/docker-haproxy-certbot/scripts/bootstrap.sh
@@ -7,6 +7,6 @@ declare -p | grep -Ev '^declare -[[:alpha:]]*r' > /container.env
 
 # go!
 
-#/certs.sh && supervisord -c /etc/supervisord.conf -n
-supervisord -c /etc/supervisord.conf -n
-/certs.sh
+/certs.sh && supervisord -c /etc/supervisord.conf -n
+#supervisord -c /etc/supervisord.conf -n
+#/certs.sh
