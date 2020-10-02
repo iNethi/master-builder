b33bab2b4ce6e172c30e7c261bc263217f2e4fa9
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-10-02 20:43:26 +0200

    test haproxyssl

--- haproxyssl/docker-haproxy-certbot/scripts/bootstrap.sh
+++ haproxyssl/docker-haproxy-certbot/scripts/bootstrap.sh
@@ -6,6 +6,4 @@
 declare -p | grep -Ev '^declare -[[:alpha:]]*r' > /container.env
 
 # go!
-
-#/certs.sh && supervisord -c /etc/supervisord.conf -n
-supervisord -c /etc/supervisord.conf -n
+/certs.sh  && supervisord -c /etc/supervisord.conf -n
--- haproxyssl/docker-haproxy-certbot/scripts/certs.sh
+++ haproxyssl/docker-haproxy-certbot/scripts/certs.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-certbot certonly --manual --no-self-upgrade --keep \
- --preferred-challenges=dns --email inethi4us@gmail.com \
- --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.inethi.net
 
-mkdir -p /etc/haproxy/certs
-for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
-    cat /etc/letsencrypt/live/$site/privkey.pem \
-        /etc/letsencrypt/live/$site/fullchain.pem \
-        | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
-done
-
+if [ -n "$CERTS" ]; then
+    certbot certonly --manual --no-self-upgrade --keep \
+    --preferred-challenges=dns --email "$EMAIL"  \
+    --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d "$CERTS" \
+    || exit 1
+    
+    mkdir -p /etc/haproxy/certs
+    for site in `ls -1 /etc/letsencrypt/live | grep -v ^README$`; do
+        cat /etc/letsencrypt/live/$site/privkey.pem \
+          /etc/letsencrypt/live/$site/fullchain.pem \
+          | tee /etc/haproxy/certs/haproxy-"$site".pem >/dev/null
+    done
+fi
 
 exit 0
