753f5ef43a1010a1200df4a0633f4c67760a928a
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-10-02 22:30:26 +0200

    test haproxyssl 13

--- haproxyssl/docker-haproxy-certbot/scripts/bootstrap.sh
+++ haproxyssl/docker-haproxy-certbot/scripts/bootstrap.sh
@@ -6,5 +6,5 @@
 declare -p | grep -Ev '^declare -[[:alpha:]]*r' > /container.env
 
 # go!
-#/certs.sh  && supervisord -c /etc/supervisord.conf -n
-supervisord -c /etc/supervisord.conf -n
+/certs.sh  && supervisord -c /etc/supervisord.conf -n
+#supervisord -c /etc/supervisord.conf -n
