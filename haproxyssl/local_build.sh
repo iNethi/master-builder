6dda07ed0831b5e10b09631cf24f3ec0bb17080e
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-10-02 20:43:26 +0200

    test haproxyssl

--- haproxyssl/local_build.sh
+++ haproxyssl/local_build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker-compose up -d
+# Build local docker of haproxy-cert
+docker build ./docker-haproxy-certbot -t djohnson/haproxy-certbot-wildcard2
+
+# Build the docker container (dont run in background)
+docker-compose up 
+
+# Make directory for letsecnrypt and haproxy cfg
+mkdir -p /mnt/data/haproxyssl
+cp ./haproxy.cfg /mnt/data/haproxyssl
+cp -ra ./letsencrypt /mnt/data/haproxyssl
+
