1bc4dd143717a31af751d548e15b5fa5b10fe790
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-10-02 20:48:17 +0200

    test haproxyssl 2

--- haproxyssl/aws_build.sh
+++ haproxyssl/aws_build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-HAPROXYSSL_IPV4=172.18.0.25 docker-compose up -d
+# Build local docker of haproxy-cert
+docker build ./docker-haproxy-certbot -t djohnson/haproxy-certbot-wildcard-test
+
+# Build the docker container (dont run in background)
+HAPROXYSSL_IPV4=172.18.0.60 docker-compose up 
+
+# Make directory for letsecnrypt and haproxy cfg
+mkdir -p /mnt/data/haproxyssl
+cp ./haproxy.cfg /mnt/data/haproxyssl
+cp -ra ./letsencrypt /mnt/data/haproxyssl
