d0f462283a9915c98ca2985e457e8494d06065af
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-10-08 13:18:47 +0200

    updated splash

--- nextcloud_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-  
-rclone -v  sync webdav:/Rshare /mnt/data/nextcloud/Rshare/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
-rclone -v  sync webdav:/Rvideo /mnt/data/nextcloud/Rvideo/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
-rclone -v  sync webdav:/Rmusic /mnt/data/nextcloud/Rmusic/   2>&1 | grep INFO | grep 'Delete\|Copied' >> /var/log/rclone.log
