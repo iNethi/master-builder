3758c9b31ec6225949421892441adb2e4e315805
David Lloyd Johnson <djohnson@cs.uct.ac.za>
2020-11-06 00:03:00 +0200

    added menus to build

--- aws_build_all.sh
+++ aws_build_all.sh
@@ -1,17 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
+# customize with your own.
+options=("Ubiquity unifi" "Ubiquity unnms" "avideo" "nextcloud" "jellyfin")
+
+menu() {
+    echo "Avaliable options:"
+    for i in ${!options[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
+    done
+    if [[ "$msg" ]]; then echo "$msg"; fi
+}
+
+prompt="Select which iNethi components you want (again to uncheck, ENTER when done): "
+while menu && read -rp "$prompt" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#options[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); 
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+done
+
+printf "You selected"; msg=" nothing"
+for i in ${!options[@]}; do 
+    [[ "${choices[i]}" ]] && { 
+        printf " %s" "[${options[i]}]"; msg=""; 
+    }
+done
+
+echo "$msg"
+echo 
+printf "Starting to build dockers ... "
+echo 
+
+printf "Create docker bridge: inethi-bridge ..."
+echo 
 sudo docker network create --attachable -d bridge --subnet=172.18.0.0/16  inethi-bridge
 #
 # Need to add HA Proxy here
 
+[[ "${choices[0]}" ]] && {
+    printf "Building Ubiquity UNMS docker ... "
+    cd ./unms
+    ./aws_build.sh
+}
 
+[[ "${choices[1]}" ]] && {
+    printf "Building Ubiquity Unifi Controller docker ... "
+    cd ./unificontroller
+    ./aws_build.sh
+}
 
-cd ./mysql
-./aws_build.sh
-cd ../nextcloud
-./aws_build.sh
-cd ../avideo
-./aws_build.sh
-cd ../plex
-./aws_build.sh $2
+[[ "${choices[2]}" ]] && {
+    printf "Building avideo docker ... "
+    cd ./avideo
+    ./aws_build.sh
+}
+
+[[ "${choices[3]}" ]] && {
+    printf "Building nextcloud docker ... "
+    cd ./nextcloud
+    ./aws_build.sh
+}
+
+[[ "${choices[4]}" ]] && {
+    printf "Building jellyfin docker ... "
+    cd ./jellyfin
+    ./aws_build.sh
+}
 
