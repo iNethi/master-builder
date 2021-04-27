06e1ae568f37f74132b26be23c7d713be7e85735
david Johnson <djohnson@davids-MacBook-Air.local>
2021-04-28 00:50:18 +0200

    added some more traefik inethi packages fixes 5

--- nginx-traefik/local_build.sh
+++ nginx-traefik/local_build.sh
@@ -5,5 +5,5 @@ export TRAEFIK_API_RULE=traefik.$inethiDN
 docker-compose up -d
 #docker-compose config
 
-sudo cp ../splash/generic-splash/* /mnt/data/traefik-nginx/html/
+sudo cp -r ../splash/generic-splash/* /mnt/data/traefik-nginx/html/
 sudo chmod -R 645 /mnt/data/traefik-nginx
