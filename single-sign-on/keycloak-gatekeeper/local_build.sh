33a7d503d6b9a32c5c7420237583a7375c3928e2
Keegan White <keeganthomaswhite@gmail.com>
2021-11-16 09:04:41 +0200

    Removed depreciated SSO gateway config

--- single-sign-on/keycloak-gatekeeper/local_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /mnt/data/keycloak_gatekeeper
-
-sudo cp -r ./keycloak-gatekeeper.conf /mnt/data/keycloak_gatekeeper/
-docker-compose up -d
--- single-sign-on/local_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /mnt/data/postgress_keycloak
-cd ./keycloak
-docker-compose up -d
-cd ..
