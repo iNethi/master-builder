e46ca3adca5036e913f19f9f822b5b378aa1c082
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2021-12-21 11:20:04 +0200

    added traefik with ssl 2

--- traefikssl/local_build.sh
+++ traefikssl/local_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ../root.conf
+sudo mkdir -p /mnt/data/traefikssl
 touch /mnt/data/traefikssl/acme.json && chmod 600 /mnt/data/traefikssl/acme.json
 cp -a ./traefik.toml /mnt/data/traefikssl/
 export TRAEFIK_API_RULE=traefik.$inethiDN
