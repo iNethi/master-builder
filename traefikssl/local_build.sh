0c3ecf759551ba8505583846121c001d36f2a465
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2022-01-16 22:02:49 +0200

    redone traefikssl

--- traefikssl/local_build.sh
+++ traefikssl/local_build.sh
@@ -2,8 +2,6 @@
 
 source ../root.conf
 sudo mkdir -p /mnt/data/traefikssl
-touch /mnt/data/traefikssl/acme.json && chmod 600 /mnt/data/traefikssl/acme.json
-cp -a ./traefik.toml /mnt/data/traefikssl/
 export TRAEFIK_API_RULE=traefik.$inethiDN
 docker-compose up -d
 #docker-compose config
