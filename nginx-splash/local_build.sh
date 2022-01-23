a8122f064a02144c9aa47614d35bbe373418f9e4
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2022-01-23 13:17:35 +0200

    fixed nginx 3

--- nginx-splash/local_build.sh
+++ nginx-splash/local_build.sh
@@ -4,5 +4,5 @@ source ./.env
 sudo mkdir -p $NGINX_VOLUME/html/
 docker-compose config
 docker-compose up -d
-sudo cp -r ../splash/generic-splash/* /mnt/data/$NGINX_VOLUME/html/
-sudo chmod -R 645 /mnt/data/$NGINX_VOLUME
+sudo cp -r ../splash/generic-splash/* $NGINX_VOLUME/html/
+sudo chmod -R 645 $NGINX_VOLUME
