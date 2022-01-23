a39290ef58dba9dd54e40deb8f0846b3b2fe4dc3
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2022-01-23 13:16:38 +0200

    fixed nginx 2

--- nginx-splash/local_build.sh
+++ nginx-splash/local_build.sh
@@ -4,5 +4,5 @@ source ./.env
 sudo mkdir -p $NGINX_VOLUME/html/
 docker-compose config
 docker-compose up -d
-sudo cp -r ../splash/generic-splash/* /mnt/data/traefik-nginx/html/
-sudo chmod -R 645 /mnt/data/traefik-nginx
+sudo cp -r ../splash/generic-splash/* /mnt/data/$NGINX_VOLUME/html/
+sudo chmod -R 645 /mnt/data/$NGINX_VOLUME
