388145e8ecef70ca283f79215e6d7aedb1bb6463
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2022-02-02 14:48:16 +0200

    wordpress fixes

--- wordpress/local_build.sh
+++ wordpress/local_build.sh
@@ -4,5 +4,5 @@ source ../root.conf
 source ./.env
 sudo mkdir -p $WORDPRESS_MOUNT
 sudo mkdir -p $MARIADB_MOUNT
-#docker-compose config
-docker-compose up -d
+docker-compose config
+#docker-compose up -d
