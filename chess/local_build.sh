90ecf41297817f7bc594d90afb6fbb2b080fe177
david Johnson <davidjohnson@davids-MacBook-Pro.local>
2022-03-07 00:37:47 +0200

    chess fix

--- chess/local_build.sh
+++ chess/local_build.sh
@@ -4,7 +4,7 @@ source ./.env
 mkdir -p $CHESS_VOLUME
 savepwd=`pwd`
 cd $CHESS_VOLUME
-git clone https://github.com/ornicar/lila-ws.git
+git clone https://github.com/ornicar/lila.git
 git clone https://github.com/ornicar/lila-ws.git
 cd $savepwd
 docker-compose config
