814570333b5daef18210b2b769d5ff8e050de756
Keegan White <keeganthomaswhite@gmail.com>
2022-04-04 22:45:23 +0200

    Added kiwix, ludo and scratch

---
+++ kiwix/local_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source ../root.conf
+source ./.env
+mkdir -p $KIWIX_VOLUME
+sudo cp ./data/*.zim $KIWIX_VOLUME
+cd ./data
+files=$(find *.zim)
+cd ..
+echo COMMAND=$files >> ../.env
+docker-compose config
+docker-compose up -d
