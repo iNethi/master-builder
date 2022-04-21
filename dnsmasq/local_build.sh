00a8ac243402cb9cd243c9d593308771b50e7650
david Johnson <david.lloyd.johnson@gmail.com>
2022-04-21 16:49:14 +0200

    added dnsmasq

--- dnsmasq/local_build.sh
+++ dnsmasq/local_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ../root.conf
 source ./.env
-mkdir -p /mnt/data/$DNSMASQ_VOLUME
+mkdir -p $DNSMASQ_VOLUME
 # Insert domain
 sed -i "s/XXXX/$inethiDN/g"  dnsmasq.conf 
 cp dnsmasq.conf /mnt/data/dnsmasq
