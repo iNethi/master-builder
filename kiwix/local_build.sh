037fd22bb7759b4522bd871614a5e0d7a1a2d323
david Johnson <david.lloyd.johnson@gmail.com>
2022-04-22 22:21:18 +0200

    fixed kiwix 2

--- kiwix/local_build.sh
+++ kiwix/local_build.sh
@@ -2,10 +2,10 @@
 source ../root.conf
 source ./.env
 mkdir -p $KIWIX_VOLUME
-mv ./data/*.zim $KIWIX_VOLUME
 cd ./data
 files=$(find *.zim)
+mv *.zim $KIWIX_VOLUME
 cd ..
-echo COMMAND=$files >> ../.env
+echo COMMAND=$files >> .env
 docker-compose config
 docker-compose up -d
