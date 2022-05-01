fd052bf59d8b877f732797c2f1823905f7d59a88
david Johnson <david.lloyd.johnson@gmail.com>
2022-05-01 14:17:39 +0200

    added startup build dependncies

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+
+# install all dependencies
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-compose
+
+
 # customize with your own.
 sudo mkdir -p /mnt/data
 # make sure all future data in this folder can be created as non root
