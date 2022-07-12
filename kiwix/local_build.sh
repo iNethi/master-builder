fa4e8921710abcc801494b3cd4f935845187a696
keegan white <david.lloyd.johnson@gmail.com>
2022-07-12 21:17:51 +0000

    fixed kiwix

--- kiwix/local_build.sh
+++ kiwix/local_build.sh
@@ -3,7 +3,6 @@ source ../root.conf
 source ./.env
 mkdir -p $KIWIX_VOLUME
 
-<<<<<<< HEAD
 if [ -f ./data/*.zim ] ; then
 	echo Moving zim files to iNethi folders
 	mv ./data/*.zim $KIWIX_VOLUME
@@ -16,15 +15,4 @@ if [ -f ./data/*.zim ] ; then
 else
 	echo No zim files to process - nothing to do
 fi
-=======
 
-
-
-cd ./data
-files=$(find *.zim)
-mv *.zim $KIWIX_VOLUME
-cd ..
-echo COMMAND=$files >> .env
-docker-compose config
-docker-compose up -d
->>>>>>> 41831df5937a0c1ab5aad20051c35a095795b014
